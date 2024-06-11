014c258936a66c574b75672195047338d81e92fe
AMINE BENSAID <aminebensaid66@gmail.com>
2024-06-11 12:23:09 +0100

    i wrote the code and tested it in codeforces

--- 71A.cpp
+++ 71A.cpp
@@ -1,1 +1,21 @@
-dsf
+#include <iostream>
+using namespace std;
+#include <vector>
+int main(){
+int n;
+cin>>n;
+vector<string>t(n);
+for(int i=0;i<n;i++){
+    string ch;
+    cin>>ch;
+    if(ch.length()>10){
+    t[i]=ch[0]+to_string(ch.length()-2)+ch[ch.length()-1];;}
+    else{
+        t[i]=ch;
+    }
+}
+for(int i=0;i<n;i++){
+    cout<<t[i]<<endl;
+}
+return 0;
+}
