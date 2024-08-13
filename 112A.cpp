ee11e3e21a7c94671e8893b5a8da8033db0de17b
Amine Bensaid <aminebensaid66@gmail.com>
2024-08-13 14:09:08 +0100

    +1

---
+++ 112A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string ch1,ch2;
+    cin>>ch1>>ch2;
+    transform(ch1.begin(),ch1.end(),ch1.begin(),::toupper);
+    transform(ch2.begin(),ch2.end(),ch2.begin(),::toupper);
+    if(ch1>ch2) cout<<1;
+    if(ch2>ch1)cout<<-1;
+    if(ch1==ch2)cout<<0;
+
+    return 0;
+}
