2511b6dd408ad77a0d1d6842a68ef116837cb4ec
Amine Bensaid <aminebensaid66@gmail.com>
2024-08-15 14:24:31 +0100

    submitted the code

--- 281A.cpp
+++ 281A.cpp
@@ -1,3 +1,13 @@
-//
-// Created by amine bensaid on 15/8/2024.
-//
+#include <iostream>
+using namespace std;
+#include <algorithm>
+
+int main() {
+    string ch1,ch2;
+    cin>>ch1;
+    ch2=ch1[0];
+    transform(ch2.begin(),ch2.end(),ch2.begin(),::toupper);
+    ch2=ch2+ch1.substr(1);
+    cout<<ch2;
+
+}
